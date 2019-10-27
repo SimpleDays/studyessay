cc74eee1c24f054a4b6a986de054948defefcd45
dongliang <342479980@qq.com>
2019-10-27 21:20:58 +0800

    rename google - baidu

--- envoy/demo/proxy-demo/run-baidu-proxy.sh
+++ envoy/demo/proxy-demo/run-baidu-proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker rm -f google-proxy
+docker rm -f baidu-proxy
 
-docker build -t google-proxy:v1 .
+docker build -t baidu-proxy:v1 .
 
-docker run -d --name google-proxy -p 8000:8000 -p 10000:10000 google-proxy:v1
+docker run -d --name baidu-proxy -p 8000:8000 -p 10000:10000 baidu-proxy:v1
