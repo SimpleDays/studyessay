e8aac0b88a689bc9184d1b7fffa6d3082a5baac1
0000013 <dongliang@suiyi.com.cn>
2021-04-28 18:33:08 +0800

    feat(all): 增加oop的学习

--- golang/helloworld/pkg/interview/oop_test.go
+++ golang/helloworld/pkg/interview/oop_test.go
@@ -35,3 +35,28 @@ func TestTeacher(t *testing.T) {
 	teacher := Teacher{}
 	teacher.ShowA()
 }
+
+/*
+ 上述解释： 被组合的类型People所包含的方法虽然升级成了外部类型Teacher这个组合类型的方法（一定要是匿名字段），
+ 但它们的方法(ShowA())调用时接受者并没有发生变化。此时People类型并不知道自己会被什么类型组合，
+ 当然也就无法调用方法时去使用未知的组合者Teacher类型的功能。
+*/
+
+func TestDeferPanic(t *testing.T) {
+	defer func() {
+		fmt.Println("打印前")
+	}()
+	defer func() {
+		fmt.Println("打印中")
+	}()
+	defer func() {
+		fmt.Println("打印后")
+	}()
+
+	panic("触发异常")
+}
+
+/*
+defer 是后进先出。 panic 需要等defer 结束后才会向上传递。
+出现panic恐慌时候，会先按照defer的后入先出的顺序执行，最后才会执行panic。
+*/
