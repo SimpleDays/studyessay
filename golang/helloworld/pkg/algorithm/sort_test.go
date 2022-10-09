13785324070dc08d6226d60cc061a67df532caae
dongliang <342479980@qq.com>
2022-10-09 19:45:58 +0800

    feat: 增加快速排序算法

---
+++ golang/helloworld/pkg/algorithm/sort_test.go
@@ -0,0 +1,34 @@
+/*
+ * @Author: dongliang 342479980@qq.com
+ * @Date: 2022-10-09 19:16:12
+ * @LastEditors: dongliang 342479980@qq.com
+ * @LastEditTime: 2022-10-09 19:45:27
+ * @FilePath: /helloworld/pkg/algorithm/sort_test.go
+ * @Description: 排序算法记录
+ */
+package algorithm
+
+import "testing"
+
+// 选择排序
+// 原理：i ~ N-1 上找到最小值的小标， 然后进行交换，一直这么操作下去
+// 时间复杂度 O(N²)
+func TestSelectSort(t *testing.T) {
+	intArray := []int{3, 6, 99, 1, 2, 0, 55, 789, 321, 21, 2, 6}
+
+	for i := 0; i < len(intArray)-1; i++ { //i ~ N-1
+		minIndex := i
+
+		for j := i + 1; j < len(intArray); j++ { // i+1 ~ N 找到比当前位置更小的数的索引位
+			if intArray[j] < intArray[minIndex] {
+				minIndex = j
+			}
+		}
+
+		tmp := intArray[i]
+		intArray[i] = intArray[minIndex]
+		intArray[minIndex] = tmp
+	}
+
+	t.Log(intArray)
+}
