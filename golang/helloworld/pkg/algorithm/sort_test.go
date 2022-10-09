e553f930a7303e8e6a5a2ca3d44c87d92f4b0488
dongliang <342479980@qq.com>
2022-10-09 23:05:47 +0800

    refactor: 增加空间复杂度描述

--- golang/helloworld/pkg/algorithm/sort_test.go
+++ golang/helloworld/pkg/algorithm/sort_test.go
@@ -2,7 +2,7 @@
  * @Author: dongliang 342479980@qq.com
  * @Date: 2022-10-09 19:16:12
  * @LastEditors: dongliang 342479980@qq.com
- * @LastEditTime: 2022-10-09 22:45:59
+ * @LastEditTime: 2022-10-09 23:05:22
  * @FilePath: /helloworld/pkg/algorithm/sort_test.go
  * @Description: 排序算法记录
  */
@@ -13,6 +13,7 @@ import "testing"
 // 选择排序
 // 原理：i ~ N-1 上找到最小值的小标， 然后进行交换，一直这么操作下去
 // 时间复杂度 O(N²)
+// 空间复杂度O(1)
 func TestSelectSort(t *testing.T) {
 	intArray := []int{3, 6, 99, 1, 2, 0, 55, 789, 321, 21, 2, 6}
 
@@ -25,6 +26,7 @@ func TestSelectSort(t *testing.T) {
 // 原理： 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
 // 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。
 // 时间复杂度 O(N²)
+// 空间复杂度O(1)
 func TestBubbleSort(t *testing.T) {
 	intArray := []int{3, 6, 99, 1, 2, 0, 55, 789, 333, 21, 2, 6, 12345}
 
