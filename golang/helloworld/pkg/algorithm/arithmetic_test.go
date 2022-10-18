b6faa359b03dbd85f5c16a31fa2e7e8bbd39b45f
dongliang <342479980@qq.com>
2022-10-18 13:10:26 +0800

    feat: 增加算法题go文件

---
+++ golang/helloworld/pkg/algorithm/arithmetic_test.go
@@ -0,0 +1,10 @@
+/*
+ * @Author: dongliang 342479980@qq.com
+ * @Date: 2022-10-18 12:06:14
+ * @LastEditors: dongliang 342479980@qq.com
+ * @LastEditTime: 2022-10-18 13:10:07
+ * @FilePath: /helloworld/pkg/algorithm/arithmetic_test.go
+ * @Description: 算法题目收录
+ */
+
+package algorithm
